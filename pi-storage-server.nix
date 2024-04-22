992ee9cc2d0cec892508eeea41d6d797ac0c5721
Jussi Aalto <j.v.aalto@gmail.com>
2024-04-22 20:54:54 +0300

    update storage server config

--- pi-storage-server.nix
+++ pi-storage-server.nix
@@ -1,5 +1,8 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   user = "pi";
@@ -7,16 +10,27 @@ let
   SSID = "mywifi";
   SSIDpassword = "mypassword";
   interface = "wlan0";
-  hostname = "raspi";
+  hostname = "pi-storage";
+
 in {
-  imports = ["${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/936e4649098d6a5e0762058cb7687be1b2d90550.tar.gz" }/raspberry-pi/4"];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      "${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/936e4649098d6a5e0762058cb7687be1b2d90550.tar.gz" }/raspberry-pi/4"
+    ];
+
+  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  boot.kernelParams = [
+    "console=ttyS0,115200n8"
+    "console=ttyAMA0,115200n8"
+    "console=tty0"
+  ];
 
   fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
     "/export/pool" = {
       device = "/dev/disk/by-uuid/cdb281b2-1650-4430-ad8a-25ca64d7a09d";
       fsType = "btrfs";
@@ -39,25 +53,36 @@ in {
     #};
   };
 
-  services.nfs.server = {
-    enable = true;
-    lockdPort = 4001;
-    mountdPort = 4002;
-    statdPort = 4000;
-    exports = ''
-      /export 		192.168.50.0/24(insecure,rw,sync,no_subtree_check,crossmnt,fsid=0)
-      /export/pool 	192.168.50.0/24(insecure,rw,sync,no_subtree_check)
-    '';
-  };
+  # Set your time zone.
+  time.timeZone = "Europe/Helsinki";
 
-  environment.systemPackages = with pkgs; [
-    vim
-    htop
-    wget
-    btrfs-progs
-  ];
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
 
-  services.openssh.enable = true;
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  
+
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "fi";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   users = {
     mutableUsers = false;
@@ -68,19 +93,60 @@ in {
     };
   };
 
-  # Enable GPU acceleration
-  #hardware.raspberry-pi."4".fkms-3d.enable = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    htop
+    wget
+    btrfs-progs
+  ];
 
-  services.xserver = {
-    layout = "fi";
-    #enable = true;
-    #displayManager.lightdm.enable = true;
-    #desktopManager.xfce.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  services.openssh.enable = true;
+
+  services.nfs.server = {
+    enable = true;
+    lockdPort = 4001;
+    mountdPort = 4002;
+    statdPort = 4000;
+    exports = ''
+      /export           192.168.50.0/24(insecure,rw,sync,no_subtree_check,crossmnt,fsid=0)
+      /export/pool      192.168.50.0/24(insecure,rw,sync,no_subtree_check)
+    '';
   };
 
-  console.useXkbConfig = true;
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
 
-  #hardware.pulseaudio.enable = true;
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
 
-  system.stateVersion = "22.05";
 }
+
